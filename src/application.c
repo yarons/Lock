3521779292698974bcd08af659c2846f265af4c4
Konstantin Tutsch <mail@konstantintutsch.com>
2024-10-14 18:32:25 +0200

    chore: release version 0.1.0

--- src/application.c
+++ src/application.c
@@ -116,7 +116,7 @@ static void lock_application_show_about(GSimpleAction *self,
     AdwAboutDialog *about =
         ADW_ABOUT_DIALOG(adw_about_dialog_new_from_appdata
                          (ROOT_RESOURCE(_PROJECT_ID(".metainfo.xml")),
-                          "1.0.0"));
+                          "0.1.0"));
 
     // Show version suffix
     adw_about_dialog_set_version(about, PROJECT_VERSION);
